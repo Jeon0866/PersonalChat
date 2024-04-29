1a5b1fdd87891251567851ccfa3ead6435b0ae2e
JinHwan <wjs545@naver.com>
2024-04-29 10:45:17 +0900

    Add files via upload

--- src/public/js/todo.js
+++ src/public/js/todo.js
@@ -5,16 +5,16 @@ let inputBox = document.getElementById('input-box')
 
 function addTask(){
     if(inputBox.value == ''){
-        alert('내용 작성 후 Enter를 눌러주세요.')
+        alert('내용 작성 후 Enter를 눌러주세요.');
     }
     else{
-        const task = document.createElement('li')
+        const task = document.createElement('li');
         task.textContent = inputBox.value;
-        listContainer.appendChild(task)
-        // inputBox.value = ''
-        let span = document.createElement('span')
-        span.textContent = "\u00d7"
-        task.appendChild(span)
+        listContainer.appendChild(task);
+        // inputBox.value = '';
+        let span = document.createElement('span');
+        span.textContent = "\u00d7";
+        task.appendChild(span);
         span.style.right = '0px';
     }
     inputBox.value = '';
